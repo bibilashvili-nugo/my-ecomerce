692d0144afd2f5d7ec265c57c032dfff1f121d86
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-08-28 07:35:13 -0700

    fetch product

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,11 +1,15 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
 import React from "react";
 
 const Home = () => {
   return (
     <Container className="bg-shop-light-pink">
       <HomeBanner />
+      <div className="py-10">
+        <ProductGrid />
+      </div>
     </Container>
   );
 };
